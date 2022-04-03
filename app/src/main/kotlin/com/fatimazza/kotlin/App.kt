04d834eb2654cedd6d4986e3b5ba753fbf46e538
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-04-03 15:53:51 +0700

    Kotlin Fundamental - Data Types and Variables

--- app/src/main/kotlin/com/fatimazza/kotlin/App.kt
+++ app/src/main/kotlin/com/fatimazza/kotlin/App.kt
@@ -11,9 +11,38 @@ class App {
 }
 
 fun main() {
-    val name = "Codewithza"
-    println(name)
-    println(if (true) "Always true" else "False")
+    // String
+    val name = "Codewithza "
+    val lastname: String = "here"
+    println(name + lastname)
 
+    // Int
+    val valueA: Int = 20
+    val valueB = 30
+    println(valueA + valueB)
+
+    // Char
+    var alphabet: Char = 'A'
+    println(alphabet +" " +alphabet++ +" " +alphabet++)
+
+    // String - Advanced Manipulation
+    val firstChar = name[0]
+    println("First character is $firstChar")
+    // Escaped String
+    println("Kotlin \nIt\'s... \t \"Awesome\"")
+    println( "Unicode text: \u00A9")
+    // Raw String
+    val line = """
+        Line 1
+        Line 2
+        Line 3
+    """.trimIndent()
+    println(line)
+
+    println("")
+
+    // Expression
+    println(if (true) "Always true" else "False")
+    // Function
     println(App().greeting)
 }
